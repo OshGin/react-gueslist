3f4c4374d32f2c200fc7cf9b73f7f66824217c4d
Mathias Angerer <>
2022-01-26 16:58:18 +0100

    Get list function, fix input, fix CSS

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 import './App.css';
 import { useState } from 'react';
 
+function List({ children }) {
+  return <ul style={{ backgroundColor: 'white' }}>{children}</ul>;
+}
+
 function App() {
   const [firstName, setFirstname] = useState('');
   const [lastName, setLastname] = useState('');
+  const index = 1; //change to ID
+  const guest = { firstName, lastName };
+  //const fullName =
   //const [attButton, setAttbutton] = false;
+  //const (submit button) ?????
 
   return (
     <div>
-      <h1>GuestList</h1>
-      <label>
-        First Name{' '}
-        <input
-          type="string"
-          value={firstName}
-          onChange={(event) => {
-            // 4. Update state variable with new value (text user types in)
-            setFirstname(event.currentTarget.value);
+      <h1>Guest List</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label>
+          First name:
+          <input
+            onChange={(e) => {
+              setFirstname(e.currentTarget.value);
+            }}
+            value={firstName}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            onChange={(e) => setLastname(e.currentTarget.value)}
+            value={lastName}
+          />
+        </label>
+
+        <button
+          onClick={() => {
+            console.log(guest);
           }}
-        />
-      </label>
-      <label>
-        Last Name{' '}
-        <input
-          type="string"
-          value={lastName}
-          onChange={(event) => {
-            // 4. Update state variable with new value (text user types in)
-            setLastname(event.currentTarget.value);
-          }}
-        />
-      </label>
+        >
+          invite is out
+        </button>
+      </form>
+      <List> new Guest</List>
     </div>
   );
 }
